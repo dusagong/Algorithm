7754ea64f8687117aa224c3cf6dc286d1a2cc44d
dusagong <yoonseungjae123@gmail.com>
2023-01-09 23:19:56 +0900

    210109

--- 백준/10799.cpp
+++ 백준/10799.cpp
@@ -2,5 +2,33 @@
 using namespace std;
 
 int main(){
-    
+    string s;
+    cin >> s;
+    int level = 0;
+    int extra = 0;
+    int swt;
+    int sum = 0;
+    for(int i = 0; i < s.length(); i++){
+        if(s[i] == '('){
+            level++;
+            swt = 1;
+            extra++;
+        }
+        else{
+            if(swt == 1){
+                level--;
+                extra--;
+                swt = 0;
+                sum += level;
+                sum += extra;
+                extra = 0;
+            }
+            else{
+                level--;
+            }
+        }
+    }
+    sum += level;
+
+    cout << sum;
 }
