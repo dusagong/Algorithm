9746fd2c00bf52c2287e4fe4e371c8e4ce08fbbd
dusagong <yoonseungjae123@gmail.com>
2023-01-10 17:59:38 +0900

    230110

---
+++ 백준/1920.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n;
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    vector<int>a;
+    int temp;
+    int ck;
+    int left, right, mid;
+    for(int i = 0; i < n; i++){
+        cin >> temp;
+        a.push_back(temp);
+    }
+    sort(a.begin(),a.end());
+   
+
+    cin >> m;
+    for(int i = 0; i < m; i++){
+        cin >> temp;
+        ck = 0;
+        left = 0;
+        right = n - 1;
+        while(left <= right){
+            mid = (left + right)/2;
+            
+            if(temp < a[mid]){
+                right = mid - 1;
+            }
+            else if(temp > a[mid]){
+                left = mid + 1;
+            }
+            else{
+                ck = 1; 
+                break;
+            }
+        }
+        if(ck == 1){
+            cout << "1\n";
+        }
+        else cout << "0\n";
+    }
+
+}
