b8a3e62a98d972e92597dd224b5735e127a57b10
dusagong <yoonseungjae123@gmail.com>
2023-01-20 21:08:36 +0900

    230120

--- 백준/15649.cpp
+++ 백준/15649.cpp
@@ -4,7 +4,5 @@ using namespace std;
 int main(){
     int n, m;
     cin >> n >> m;
-    for(int i = 0; i < n; i++){
-        
-    }
+    
 }
---
+++ 백준/1935.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    char temp = a;
+    int *arr = new int[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }   
+    for(int i = 0; i < s.length(); i++){
+        if(s[i] > 64 && s[i] < 91){
+            
+        }
+    }
+}
