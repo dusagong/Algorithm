de395419583852c97bebcef88a5eba05354a7fce
dusagong <yoonseungjae123@gmail.com>
2023-02-07 20:38:41 +0900

    1

---
+++ 백준/23879.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    int *p = new int[n];
+    int *t = new int[n];
+    int fir = 0;
+    int las = n - 1;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> p[i];
+    }
+    for(int i = 0; i < n; i++){
+        cin >> t[i];
+    }
+    
+    
+}
