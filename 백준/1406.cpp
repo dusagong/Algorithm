9adb87a6a28ba624c526a9a6a6d2a26be20ae39e
dusagong <yoonseungjae123@gmail.com>
2024-04-28 21:18:53 +0900

    .

--- 백준/1406.cpp
+++ 백준/1406.cpp
@@ -5,29 +5,32 @@ const int MX = 1000005;
 char dat[MX];
 int pre[MX], nxt[MX];
 int unused = 1;
-int cursor = 1;
+int cursor = 0;
 void insert(int addr, char num){
+  
   dat[unused] = num;
   pre[unused] = addr;
   nxt[unused] = nxt[addr]; 
-  nxt[addr] = unused;
   if(nxt[addr] != -1)pre[nxt[addr]] = unused;
+  nxt[addr] = unused;
+
+  cursor = nxt[addr];
   unused++;
-  cursor++;
 }
 
 void erase(int addr){
   nxt[pre[addr]] = nxt[addr];
   if(nxt[addr] != -1)pre[nxt[addr]] = pre[addr];
+  cursor = pre[addr];
 }
 
 void traverse(){
   int cur = nxt[0];
   while(cur != -1){
-    cout << dat[cur] << ' ';
+    cout << dat[cur];
     cur = nxt[cur];
   }
-  cout << "\n\n";
+  // cout << "\n\n";
 }
 
 int main(){
@@ -37,94 +40,45 @@ int main(){
     string s;
     cin >> s;
     for(int i = 0; i< s.length(); i++){
+        // dat[unused] = s[i];
+        // nxt[unused] = -1;
+        // pre[unused] = unused - 1;
+        // nxt[unused -1] = unused;
+        // unused++;
         dat[unused] = s[i];
         nxt[unused] = -1;
         pre[unused] = unused - 1;
-        nxt[unused -1] = unused;
+        nxt[unused - 1] = unused;
         unused++;
+        cursor++;
     }
-    #include <iostream>
-    using namespace std;
+    int n;
+    cin >> n;
+    char c;
+    char d;
+    for(int i = 0; i < n; i++){
+      cin >> c;
+      if(c == 'L'){
+        if(pre[cursor] != -1)cursor = pre[cursor];
+      }
+      else if(c == 'D'){
+        if(nxt[cursor] != -1) cursor = nxt[cursor];
 
-    // int n;
-    // cin >> n;
-    // string temp;
-    // char c;
-    // for(int i = 0; i < n; i++){
-    //     cin >> temp;
-    //     if(temp == "P"){
-    //         cin >> c;
-    //         unused--;
-    //         insert(unused,c);
-    //     }
-    //     if(temp == "L"){
-    //         unused--;
-    //     }
-    //     if(temp == "B"){
-    //         unused--;
-    //         erase(unused);
-    //     }
-    //     if(temp == "D"){
-    //         if(nxt[unused] != -1)unused++;
-    //     }
-    // }
+      }
+      else if(c == 'B'){
+        if(pre[cursor] != -1) {
+          erase(cursor);
+        }
+
+      }
+      else{
+        cin >> d;
+        insert(cursor,d);
+      }
+    }
+
     traverse();
 }
 
 
 
-
-
-
-
-// #include <iostream>
-// #include <stack>
-
-// using namespace std;
-
-// int main(){
-//     string s;
-//     int t;
-//     cin >> s >> t; //s는 문자열., t는 명령어 횟수
-//     stack<char> a;
-//     stack<char> b;
-//     string command;
-//     char k;
-//     for(int i = 0; i < s.length(); i++){
-//         a.push(s[i]);
-//     }
-//     for(int i = 0; i < t; i++){
-//         cin >> command;
-//         if(command == "L"){
-//             if(a.empty())continue;
-//             b.push(a.top());
-//             a.pop();
-//         }
-//         else if(command == "D"){
-//             if(b.empty())continue;
-
-//             a.push(b.top());
-//             b.pop();
-//         }
-//         else if(command == "B"){
-//             if(a.empty())continue;
-
-//             a.pop();
-//         }
-//         else{
-//             cin >> k;
-//             a.push(k);
-//         }
-//     }
-//     int temp = a.size();
-
-//     for(int i = 0; i < temp; i++){
-//         b.push(a.top());
-//             a.pop();
-//     }
-//     temp = b.size();
-//     for(int i = 0; i < temp; i++){
-//         cout << b.top();
-//         b.pop();
-//     }
-// }
